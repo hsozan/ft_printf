73da2cb5b4e92b68b04e7486b72d18e957f4fec7
kratoshakan <hakansozan.inci@gmail.com>
2023-01-13 15:58:07 +0300

    cix

--- ft_printf.c
+++ ft_printf.c
@@ -1,97 +1,109 @@
-#include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
 
 //ft_printf with s d x X c p u %
-
-int ft_putchar(char c)
+int	ft_putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
-int ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i])
-    {
-        ft_putchar(s[i]);
-        i++;
-    }
-    if(!s)
-        return (ft_putstr("(null)"));
-    return (i);
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+	if (!s)
+		return (ft_putstr("(null)"));
+	return (i);
 }
 
-int ft_putnbr_base(int nbr, char *base, int base_len)
+int	ft_putnbr_base(unsigned long int n, char *base_c, int base)
 {
-    int res;
+	int	res;
+	int	i;
+	int	n_backwards[100];
 
-    res = 0;
-    if (nbr < 0)
-    {
-        ft_putchar('-');
-        nbr *= -1;
-        res++;
-    }
-    if (nbr >= base_len)
-    {
-        res += ft_putnbr_base(nbr / base_len, base, base_len);
-        res += ft_putnbr_base(nbr % base_len, base, base_len);
-    }
-    else
-    {
-        ft_putchar(base[nbr]);
-        res++;
-    }
-    return (res);
-}
-int ft_sort(const char *s, va_list args)
-{
-    int res;
-
-    res = 0;
-    if (*s == 's')
-        return (ft_putstr(va_arg(args, char *)));
-    else if (*s == 'd')
-        return (ft_putnbr_base(va_arg(args, int), "0123456789", 10));
-    else if (*s == 'x')
-        return (ft_putnbr_base(va_arg(args, int), "0123456789abcdef", 16));
-    else if (*s == 'X')
-        res += ft_putnbr_base(va_arg(args, int), "0123456789ABCDEF", 16);
-    else if (*s == 'c')
-        res += ft_putchar(va_arg(args, int));
-    else if (*s == 'p')
-        return (ft_putstr("0x") + ft_putnbr_base(va_arg(args, int), "0123456789abcdef", 16));
-    else if (*s == 'u')
-        res += ft_putnbr_base(va_arg(args, unsigned int), "0123456789", 10);
-    else if (*s == '%')
-        res += ft_putchar('%');
+	res = 0;
+	i = 0;
+	if (n < 0)
+	{
+		res += ft_putchar('-');
+		n *= -1;
+	}
+	else if (n == 0)
+		return (ft_putchar('0'));
+	while (n)
+	{
+		n_backwards[i] = n % base;
+		n /= base;
+		i++;
+	}
+	while (i--)
+		res += ft_putchar(base_c[n_backwards[i]]);
+	return (res);
 }
 
-int ft_printf(const char *s, ...)
+int	ft_sort(char c, va_list args)
 {
-    va_list args;
-    int i;
-    int res;
-
-    i = 0;
-    res = 0;
-    va_start(args, s);
-    while (s)
-    {
-        if (*s == '%')
-        {
-            res += ft_sort(s, args);
-        }
-        else
-        {
-            write(1, &s[i], 1);
-            res++;
-        }
-        s++;
-    }
-    va_end(args);
-    return (res);
+	if (c == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (c == 'd')
+		return (ft_putnbr_base(va_arg(args, int), "0123456789", 10));
+	else if (c == 'x')
+		return (ft_putnbr_base(va_arg(args, unsigned long int), \
+				"0123456789abcdef", 16));
+	else if (c == 'X')
+		return (ft_putnbr_base(va_arg(args, unsigned long int), \
+					"0123456789ABCDEF", 16));
+	else if (c == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (c == 'p')
+		return (ft_putstr("0x") + ft_putnbr_base(va_arg(args, \
+					unsigned long int), "0123456789abcdef", 16));
+	else if (c == 'u')
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789", 10));
+	else if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
 }
+
+int	ft_printf(const char *s, ...)
+{
+	va_list	args;
+	int		i;
+	int		res;
+
+	i = 0;
+	res = 0;
+	va_start(args, s);
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+			i++;
+			res += ft_sort(s[i], args);
+			i++;
+		}
+		else
+		{
+			res += ft_putchar(s[i]);
+			i++;
+		}
+	}
+	va_end(args);
+	return (res);
+}
+/*#include <stdio.h>
+int main()
+{
+    int a = 12;
+    ft_printf("Hello %s %d %x %X %c %p %u %%\n", "World", a, a, a, 'A', &a, 42);
+    printf("Hello %s %d %x %X %c %p %u %%\n", "World", a, a, a, 'A', &a, 42);
+
+    return 0;
+}*/
