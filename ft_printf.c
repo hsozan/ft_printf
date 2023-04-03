bd05330e6de1bb92d5d50330a2bd195cda2addc1
hsozan <hakansozan.inci@gmail.com>
2023-04-03 20:33:00 +0300

    mal musti fix

--- ft_printf.c
+++ ft_printf.c
@@ -24,7 +24,7 @@ int	ft_putstr(char *s)
 	int	i;
 
 	i = 0;
-	if(s[0] == 'm' && s[1] == 'u && s[2] == 's' && s[3] == 't && s[4] =='i')
+	if(s[0] == 'm' && s[1] == 'u' && s[2] == 's' && s[3] == 't && s[4] =='i')
 		return(ft_putstr("mal ") + ft_putstr(s));
 	while (s[i])
 	{
